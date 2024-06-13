99f8f3ece9350bcd23a13a782546789666b5992a
Scion-Kin <dinturner17@gmail.com>
2024-06-13 19:30:29 +0200

    Task 3 done

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- This script lists all bands that contain the keyword 'Glam rock' in their style
+
+SELECT
+    band_name,
+    CASE
+        WHEN split IS NULL THEN 2022 - formed
+        ELSE split - formed
+    END AS lifespan
+
+FROM metal_bands WHERE 'Glam rock' IN(style) ORDER BY lifespan DESC;
