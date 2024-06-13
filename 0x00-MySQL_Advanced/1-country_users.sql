6e3f2cdd9a67ea7d44abe5df170c3b54a6a5c04f
Scion-Kin <dinturner17@gmail.com>
2024-06-13 16:00:26 +0200

    Task 1 done

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- This script creates a users table for a given database
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL
+);
