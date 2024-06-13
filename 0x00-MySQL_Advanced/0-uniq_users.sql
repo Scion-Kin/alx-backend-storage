082c47a691daf69c3d8326de51ddd911ef69a7b2
Scion-Kin <dinturner17@gmail.com>
2024-06-13 13:34:12 +0200

    Task 0 done

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,7 @@
 -- This creates a users table for any database
 
-CREATE TABLE IF NOT EXISTS `users` (`id` INT AUTOINCREMENT NOT NULL PRIMARY KEY,
-`email` VARCHAR(255) NOT NULL UNIQUE, `name` VARCHAR(255))
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
+);
