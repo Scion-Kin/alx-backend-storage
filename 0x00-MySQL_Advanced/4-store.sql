f95f71f53ed76ad16d191fa8536a23e2ae1f824a
Scion-Kin <dinturner17@gmail.com>
2024-06-13 20:26:55 +0200

    Task 4 done

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,6 +5,6 @@ CREATE TRIGGER `decrease` AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - 1
+    SET quantity = quantity - NEW.number
     WHERE items.name = NEW.item_name;
 END$$
