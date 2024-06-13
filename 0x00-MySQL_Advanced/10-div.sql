1b21acd42c9217b027d70ac2b572e3ffd201a77d
Scion-Kin <dinturner17@gmail.com>
2024-06-13 23:20:22 +0200

    Task 10 done

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -5,8 +5,10 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF b == 0
-        RETURN 0
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
     END IF;
-    RETURN a / b;
+    RETURN result;
 END$$
